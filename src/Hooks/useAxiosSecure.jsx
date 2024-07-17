1c201b2ce5a37ec6edd18f80240ff905caf4ce64
s-shahriar <ksnkkc@gmail.com>
2024-07-17 19:25:30 +0600

    interceptor fix

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -27,7 +27,7 @@ const useAxiosSecure = () => {
     },
     async (error) => {
       const status = error.response.status;
-      console.log('status error in the interceptor', status);
+      //console.log('status error in the interceptor', status);
       if (status === 401 || status === 403) {
         await logOut()
         setLoading(false)
